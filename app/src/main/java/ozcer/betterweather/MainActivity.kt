0e0b9ca6a3a3840c6fbcd3dac3c7f253c685c939
ozcer <iamreallyoscar@hotmail.com>
2017-11-29 17:45:57 -0500

    added new fonts imports anko main activity makes simple request to server

--- app/src/main/java/ozcer/betterweather/MainActivity.kt
+++ app/src/main/java/ozcer/betterweather/MainActivity.kt
@@ -2,11 +2,29 @@ package ozcer.betterweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    val apiUrl: String = "http://api.wunderground.com/api/71cdf3c3260ce0f2/conditions/q/zmw:00000.176.71508.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doAsync {
+            val Toronto: JSONObject = JSONObject(URL(apiUrl).readText())
+            val currentObservation = Toronto.getJSONObject("current_observation")
+            val temp = currentObservation.getDouble("temp_c")
+            val msg = "Toronto is currently ${temp.toString()}C"
+            uiThread {
+                textTxt.setText(msg)
+            }
+        }
     }
 }
