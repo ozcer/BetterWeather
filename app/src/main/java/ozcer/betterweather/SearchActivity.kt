ee7035536954abc2c8dcf01f93881c86a6fa7413
ozcer <iamreallyoscar@hotmail.com>
2017-11-30 13:20:50 -0500

    now show dummy list in search activity

--- app/src/main/java/ozcer/betterweather/SearchActivity.kt
+++ app/src/main/java/ozcer/betterweather/SearchActivity.kt
@@ -2,7 +2,10 @@ package ozcer.betterweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_search.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
@@ -10,24 +13,27 @@ import org.jetbrains.anko.longToast
 class SearchActivity : AppCompatActivity() {
 
     val searchQueryBase = "http://autocomplete.wunderground.com/aq?query="
+    val searchResults = listOf("Vancouver", "New York")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
 
         searchFieldEdt.setOnEditorActionListener() {v, actionId, event ->
             if(actionId == EditorInfo.IME_ACTION_DONE){
                 val city = searchFieldEdt.text.toString()
                 searchCity(city)
                 true
-            } else {
-                false
-            }
-
+            } else { false }
         }
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults)
+        searchResultLv.adapter = adapter
+
     }
 
     fun searchCity(city: String){
-        longToast("u pressed enter")
+        
     }
 }
---
+++ app/src/main/java/ozcer/betterweather/SearchResult.kt
@@ -0,0 +1,8 @@
+package ozcer.betterweather
+
+/**
+ * Created by ozcer on 2017-11-30.
+ */
+class SearchResult(rowName: String) {
+    val name = rowName
+}
