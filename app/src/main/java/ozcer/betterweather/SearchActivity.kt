b960ad59b99b7406d5978cada84df8deee16ea5b
ozcer <iamreallyoscar@hotmail.com>
2017-11-30 15:19:07 -0500

    hooked up with API to display search result

--- app/src/main/java/ozcer/betterweather/SearchActivity.kt
+++ app/src/main/java/ozcer/betterweather/SearchActivity.kt
@@ -2,23 +2,32 @@ package ozcer.betterweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_search.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
+import java.net.URL
+import android.widget.BaseAdapter
+
+
 
 class SearchActivity : AppCompatActivity() {
 
     val searchQueryBase = "http://autocomplete.wunderground.com/aq?query="
-    val searchResults = listOf("Vancouver", "New York")
+    var searchResults = ArrayList<String>(0)
+    lateinit var listAdapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults)
+        searchResultLv.adapter = listAdapter
 
         searchFieldEdt.setOnEditorActionListener() {v, actionId, event ->
             if(actionId == EditorInfo.IME_ACTION_DONE){
@@ -28,12 +37,21 @@ class SearchActivity : AppCompatActivity() {
             } else { false }
         }
 
-        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults)
-        searchResultLv.adapter = adapter
-
     }
 
     fun searchCity(city: String){
-        
+        doAsync {
+            val returnString = URL(searchQueryBase+city).readText()
+            val resultList = JSONObject(returnString).getJSONArray("RESULTS")
+            //Log.i("doa", "jsonarray: $resultList")
+            uiThread {
+                searchResults.clear()
+                for (i in 0..resultList.length() -1) {
+                    val entry = resultList.getJSONObject(i).getString("name")
+                    searchResults.add(entry)
+                }
+                listAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
